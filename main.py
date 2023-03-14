2a0deabd1f5b3e6b769a1283d22efea9346fc442
Albondigaesparadrapo <124705845+Albondigaesparadrapo@users.noreply.github.com>
2023-03-14 17:57:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import streamlit as st
 import requests
 import json
 
-if st.button("Generate meme"):
-    url = "https://meme-api.com/gimme/memes"
-    rspt = requests.get(url)
-    data = json.loads(rspt.text)
-    meme = data["url"]
-    memetitle = data["title"]
-    memeauthor = data["author"]
-    st.header(memetitle)
-    st.markdown("---")
-    st.subheader("Submitted by "+memeauthor)
-    st.markdown("![Our meme]("+meme+")")
+subreddit = st.text_input
+if subreddit != "":    
+    if st.button("Generate meme"):
+        url = "https://meme-api.com/gimme/"+subreddit
+        rspt = requests.get(url)
+        data = json.loads(rspt.text)
+        meme = data["url"]
+        memetitle = data["title"]
+        memeauthor = data["author"]
+        st.header(memetitle)
+        st.markdown("---")
+        st.subheader("Submitted by "+memeauthor)
+        st.markdown("![Our meme]("+meme+")")
