0856cd574745304246f1d64967d7ea5c0ff813c8
Albondigaesparadrapo <124705845+Albondigaesparadrapo@users.noreply.github.com>
2023-03-14 17:37:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+import json
+
+if st.button("Generate meme"):
+    url = "https://meme-api.com/gimme/memes"
+    rspt = requests.get(url)
+    data = json.loads(rspt.text)
+    meme = data["url"]
+    memetitle = data["title"]
+    memeauthor = data["author"]
+    st.header(memetitle)
+    st.markdown("---")
+    st.subheader(memeauthor)
+    st.markdown("![Our meme]("+meme+")")
